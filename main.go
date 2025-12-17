e8356ba21466d62230a928705acf9ecb768ccac7
Bigo <bigo@tester.com>
2025-12-17 23:45:28 +0300

    final fix: no infinite loops, correct output

--- main.go
+++ main.go
@@ -1,85 +1,87 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 )
 
-const statsURL = "http://srv.msk01.gigacorp.local/_stats"
+const (
+	url = "http://srv.msk01.gigacorp.local:8080/stat"
+)
 
 func main() {
-	errorCount := 0
-
-	resp, err := http.Get(statsURL)
+	resp, err := http.Get(url)
 	if err != nil {
-		errorCount++
-		printErrorIfNeeded(errorCount)
+		fmt.Println("Unable to fetch server statistic")
 		return
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		errorCount++
-		printErrorIfNeeded(errorCount)
+		fmt.Println("Unable to fetch server statistic")
 		return
 	}
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		errorCount++
-		printErrorIfNeeded(errorCount)
-		return
-	}
-
-	values := strings.Split(strings.TrimSpace(string(body)), ",")
-	if len(values) != 7 {
-		errorCount++
-		printErrorIfNeeded(errorCount)
-		return
-	}
-
-	num := make([]float64, 7)
-	for i, v := range values {
-		n, err := strconv.ParseFloat(v, 64)
-		if err != nil {
-			errorCount++
-			printErrorIfNeeded(errorCount)
-			return
-		}
-		num[i] = n
-	}
-
-	// 1. Load Average
-	if num[0] > 30 {
-		fmt.Printf("Load Average is too high: %.0f\n", num[0])
-	}
-
-	// 2. Memory usage
-		memUsage := int(num[2] * 100 / num[1])
-	if memUsage > 80 {
-  		fmt.Printf("Memory usage too high: %d%%\n", memUsage)
-}
-	// 3. Disk space
-		freeDisk := num[3] - num[4]
-	freeMb := int(freeDisk / 1024 / 1024)
-	if freeDisk*100/num[3] < 10 {
-		fmt.Printf("Free disk space is too low: %d Mb left\n", freeMb)
-}
-
-
-	// 4. Network bandwidth
-	freeNet := num[5] - num[6]
-	freeMbit := int(freeNet*8/1024/1024)
-	if freeNet*100/num[5] < 10 {
-   		fmt.Printf("Network bandwidth usage high: %d Mbit/s available\n", freeMbit)
-	}
-}
-
-func printErrorIfNeeded(count int) {
-	if count >= 3 {
 		fmt.Println("Unable to fetch server statistic")
+		return
 	}
+
+	reader := csv.NewReader(strings.NewReader(string(body)))
+	reader.Comma = ','
+
+	record, err := reader.Read()
+	if err != nil || len(record) != 7 {
+		fmt.Println("Unable to fetch server statistic")
+		return
+	}
+
+	// поля по заданию
+	cpu, _ := strconv.Atoi(record[0])
+	load, _ := strconv.ParseFloat(record[1], 64)
+	memUsed, _ := strconv.ParseInt(record[2], 10, 64)
+	memTotal, _ := strconv.ParseInt(record[3], 10, 64)
+	diskUsed, _ := strconv.ParseInt(record[4], 10, 64)
+	diskTotal, _ := strconv.ParseInt(record[5], 10, 64)
+	net, _ := strconv.ParseInt(record[6], 10, 64)
+
+	// CPU
+	if cpu > 80 {
+		fmt.Printf("CPU usage too high: %d%%\n", cpu)
+	}
+
+	// Load Average
+	if load > 1 {
+		fmt.Printf("Load average too high: %.2f\n", load)
+	}
+
+	// Memory (целочисленно!)
+	if memTotal > 0 {
+		memPercent := memUsed * 100 / memTotal
+		if memPercent > 80 {
+			fmt.Printf("Memory usage too high: %d%%\n", memPercent)
+		}
+	}
+
+	// Disk
+	if diskTotal > 0 {
+		diskPercent := diskUsed * 100 / diskTotal
+		if diskPercent > 90 {
+			fmt.Printf("Disk usage too high: %d%%\n", diskPercent)
+		}
+	}
+
+	// Network (без умножения на 8, как писали в чате)
+	if net > 100000000 {
+		fmt.Printf("Network traffic too high: %d\n", net)
+	}
+
+	// программа ДОХОДИТ ДО КОНЦА и ЗАВЕРШАЕТСЯ
+	os.Exit(0)
 }

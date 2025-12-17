fab29b4c6eeb129796dd5bf64b06e9ba8e653e9a
Bigo <bigo@tester.com>
2025-12-17 23:05:29 +0300

    add http client skeleton

--- main.go
+++ main.go
@@ -1,3 +1,49 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+const statsURL = "http://srv.msk01.gigacorp.local/_stats"
+
+func main() {
+	resp, err := http.Get(statsURL)
+	if err != nil {
+		fmt.Println("Unable to fetch server statistic")
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Unable to fetch server statistic")
+		return
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Unable to fetch server statistic")
+		return
+	}
+
+	values := strings.Split(strings.TrimSpace(string(body)), ",")
+	if len(values) != 7 {
+		fmt.Println("Unable to fetch server statistic")
+		return
+	}
+
+	numbers := make([]float64, 7)
+	for i, v := range values {
+		n, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			fmt.Println("Unable to fetch server statistic")
+			return
+		}
+		numbers[i] = n
+	}
+
+	// дальше будет логика проверок — добавим следующим шагом
+}

0e366a5504f697d91caa3fea3df734b166c2c947
Bigo <bigo@tester.com>
2025-12-17 23:56:01 +0300

    final_1

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ import (
 	"strings"
 )
 
-const url = "http://srv.msk01.gigacorp.local:8080/stat"
+const url = "http://srv.msk01.gigacorp.local/_stats"
 
 func main() {
 	for {
 		resp, err := http.Get(url)
 		if err != nil {
-			// сервер выключился — НОРМАЛЬНО выходим
+			// сервер завершился — корректно выходим
 			return
 		}
 
@@ -36,15 +36,18 @@ func main() {
 			return
 		}
 
-		cpu, _ := strconv.Atoi(record[0])
-		load, _ := strconv.Atoi(strings.Split(record[1], ".")[0])
-		memUsed, _ := strconv.ParseInt(record[2], 10, 64)
-		memTotal, _ := strconv.ParseInt(record[3], 10, 64)
-		diskUsed, _ := strconv.ParseInt(record[4], 10, 64)
-		diskTotal, _ := strconv.ParseInt(record[5], 10, 64)
-		net, _ := strconv.ParseInt(record[6], 10, 64)
+		load, _ := strconv.Atoi(strings.Split(record[0], ".")[0])
 
-		if load > 50 {
+		memTotal, _ := strconv.ParseInt(record[1], 10, 64)
+		memUsed, _ := strconv.ParseInt(record[2], 10, 64)
+
+		diskTotal, _ := strconv.ParseInt(record[3], 10, 64)
+		diskUsed, _ := strconv.ParseInt(record[4], 10, 64)
+
+		netTotal, _ := strconv.ParseInt(record[5], 10, 64)
+		netUsed, _ := strconv.ParseInt(record[6], 10, 64)
+
+		if load > 30 {
 			fmt.Printf("Load Average is too high: %d\n", load)
 		}
 
@@ -55,14 +58,17 @@ func main() {
 			}
 		}
 
-		if net > 0 {
-			fmt.Printf("Network bandwidth usage high: %d Mbit/s available\n", net/1_000_000)
+		if diskTotal > 0 {
+			freeMb := (diskTotal - diskUsed) / (1024 * 1024)
+			if freeMb*100/diskTotal < 10 {
+				fmt.Printf("Free disk space is too low: %d Mb left\n", freeMb)
+			}
 		}
 
-		if diskTotal > 0 {
-			free := (diskTotal - diskUsed) / (1024 * 1024)
-			if free < 50000 {
-				fmt.Printf("Free disk space is too low: %d Mb left\n", free)
+		if netTotal > 0 {
+			freeMbit := (netTotal - netUsed) / 1_000_000
+			if netUsed*100/netTotal > 90 {
+				fmt.Printf("Network bandwidth usage high: %d Mbit/s available\n", freeMbit)
 			}
 		}
 	}
